9aafb6422805e39fb365d962f638918b3d06483d
Gabriel José de Melo <gabriel.melo@ftt-20L.engcomp.local>
2019-08-29 13:51:52 -0300

    Modificações no css e js de armadilha

--- js/Modal.js
+++ js/Modal.js
@@ -4,10 +4,10 @@ class Modal {
         const modal = document.getElementById(modalId)
 
         if(modal) {
-            modal.classList.add('modalAberto')
+            modal.style.display = 'flex'
             modal.addEventListener('click', e => {
                 if(e.target.id === modalId || e.target.className === "fecharModal") {
-                    modal.classList.remove('modalAberto')
+                    modal.style.display = 'none'
                 }
             })
         }
--- js/armadilhas.js
+++ js/armadilhas.js
@@ -1,4 +1,17 @@
 const modal = new Modal()
 
+// Evento para abrir e fechar o modal
 const btn = document.getElementById('addArmadilha')
 btn.addEventListener('click', () => modal.iniciarModal('addArmadilhaModal'))
+
+// Evento botão de adicionar testes de resistência
+const addTeste = document.querySelector('#adicionarTestes')
+const addTesteBtn = document.querySelector('#adicionarTestes header button')
+
+/*
+	Funções
+*/
+
+addTesteBtn.addEventListener('click', (e) => {
+	addTeste.innerHTML += e.target
+})
