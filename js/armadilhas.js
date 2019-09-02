adebc70458abdd3244aff0630b48a7144a3e64dc
Gabriel José de Melo <gabriel.melo@ftt-20L.engcomp.local>
2019-09-02 13:46:52 -0300

    Adicionei o local para cadastro dos teste de resistência

--- js/armadilhas.js
+++ js/armadilhas.js
@@ -5,20 +5,29 @@ const btn = document.getElementById('addArmadilha')
 btn.addEventListener('click', () => modal.iniciarModal('addArmadilhaModal'))
 
 // Evento botão de adicionar testes de resistência
-const addTeste = document.querySelector('#adicionarTestes')
+const addTeste = document.querySelector('#localTestes')
 const addTesteBtn = document.querySelector('#adicionarTestes header button')
-
-/*
-	Funções
-*/
 
 addTesteBtn.addEventListener('click', (e) => {
 	e.preventDefault()
-	add()
+	addTesteDeResistencia()
 })
 
-function add() {
-	const p = document.createElement('p')
-	p.innerHTML = "Hello World"
-	addTeste.appendChild(p)
+/*** Funções ***/
+
+function addTesteDeResistencia() {
+	const div = document.createElement('div')
+	div.classList.add('localTeste')
+	div.innerHTML = `
+	<label>Nome
+		<input type="text" placeholder="Nome do Teste">
+	</label>
+	<label> CD
+		<input type="text" placeholder="Classe de Dificuldade">
+	</label>
+	<label>Tipo de Dano
+		<input type="text" placeholder="Dano caso falhe">
+	</label>
+	<button class="excluirTeste">X</button>`
+	addTeste.appendChild(div)
 }
